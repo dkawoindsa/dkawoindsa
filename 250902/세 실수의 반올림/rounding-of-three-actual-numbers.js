9df15e5a5e027077b6b94c3f5e23805085f66cb1
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2025-09-02 15:11:45 +0900

    Title: 세 실수의 반올림, Time: 70ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250902/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+let nums = fs.readFileSync(0).toString().trim().split('\n');
+
+console.log(Math.round(nums[0] * 1000) / 1000)
+console.log(Math.round(nums[1] * 1000) / 1000)
+console.log((Math.round(nums[2] * 1000) / 1000).toFixed(3))
