0a60a12f8de78ca35a7a62d012cf259f1865acab
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2025-09-02 15:21:39 +0900

    Title: 합과 평균, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 250902/합과 평균/sum-and-avg.js
+++ 250902/합과 평균/sum-and-avg.js
@@ -4,4 +4,4 @@ let nums = fs.readFileSync(0).toString().trim().split(' ');
 let A = Number(nums[0]);
 let B = Number(nums[1]);
 
-console.log( A + B, (A + B) /2)
+console.log( A + B, ((A + B) / 2).toFixed(1))
