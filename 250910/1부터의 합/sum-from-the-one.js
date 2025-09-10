862670792ddd47ba4e804f307f45c80a7a638d79
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2025-09-10 14:42:49 +0900

    Title: 1부터의 합, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250910/1부터의 합/sum-from-the-one.js
+++ 250910/1부터의 합/sum-from-the-one.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 let N = fs.readFileSync(0).toString().trim();
 let sum = 0;
-for (let i = 0; i <= 100; i++) {
+
+for (let i = 1; i <= 100; i++) {
+     sum += i;
     if (sum > N) {
         console.log(i);
         break;
     }
-    sum += i;
 }
