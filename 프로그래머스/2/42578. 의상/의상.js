db8eebd9c528475e751a28bc8cdf8c880323ab96
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-03-18 16:52:26 +0900

    [level 2] Title: 의상, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,15 @@
+function solution(clothes) {
+    let answer = 1;
+    let clothesObj = {};
+    
+    for (let [name, category] of clothes) {
+        clothesObj[category] = (clothesObj[category] || 0) + 1;
+    }
+    
+    for (let category in clothesObj) {
+        answer *= (clothesObj[category] + 1)
+    }
+    
+    
+    return answer - 1;
+}
