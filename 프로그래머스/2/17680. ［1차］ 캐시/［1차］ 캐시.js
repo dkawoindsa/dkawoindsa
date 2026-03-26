a90966d2236883e6bdd36c9f667aef7e2ab99341
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-03-26 16:51:41 +0900

    [level 2] Title: [1차] 캐시, Time: 39.98 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -4,28 +4,21 @@ function solution(cacheSize, cities) {
     
     cities = cities.map(city => city.toUpperCase());
     
-     if (cacheSize === 0) return cities.length * 5;
+    if (cacheSize === 0) return cities.length * 5;
     
-    for (let i = 0; i < cacheSize; i++) {
-        if (cache.includes(cities[i])) {
-            cache.push(cities[i]);
-            answer++;
-        } else {
-            cache.push(cities[i]);
-            answer += 5;
-        }
-    }
-    
-    for (let i = cacheSize; i < cities.length; i++) {
-         if (cache.includes(cities[i])) {
-             let cityIndex = cache.indexOf(cities[i]);
-             cache.splice(cityIndex, 1);
-             cache.push(cities[i]);
-             answer++;
+    for (let i = 0; i < cities.length; i++) {
+        let city = cities[i];
+        
+         if (cache.includes(city)) {
+             cache.splice(cache.indexOf(city), 1);
+             cache.push(city);
+             answer += 1;
              
          } else {
-             cache.shift();
-             cache.push(cities[i]);
+             if (cache.length >= cacheSize) {
+                 cache.shift();
+             }
+             cache.push(city);
              answer += 5;
          }
     }
