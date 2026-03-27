7474bf43f9101f6625a635a7699dc9d441cf9b3f
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-03-27 18:11:39 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,11 +1,7 @@
 function solution(s) {
-    s = s.split(' ');
-    let len = s.length;
-    s.sort((a, b) => (a - b));
-
-    let answer = []
-    answer.push(s[0]);
-    answer.push(s[len - 1]);
-    
-    return answer.join(' ');
+    let answer = '';
+    s = s.split(' ').sort((a, b) => a - b);
+    answer += s[0] + ' ';
+    answer += s[s.length - 1];
+    return answer;
 }
