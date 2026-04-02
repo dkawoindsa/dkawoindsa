3188cb744b05f02a0988e18161bda1c678e246b0
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-04-02 16:05:12 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.84 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,5 +1,10 @@
 function solution(A,B){
-    A = A.sort((a, b) => a - b);
-    B = B.sort((a, b) => b - a);
-    return A.reduce((tot, val, idx) => tot + val * B[idx], 0);
+    let answer = 0;
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+    
+    for (let i = 0; i < A.length; i++) {
+        answer += A[i] * B[i];
+    }
+    return answer;
 }
