9fe4f6c2592511a3a41f64ffde1a220e8f631959
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-04-03 16:40:29 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 31.97 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,21 +1,21 @@
 function solution(s) {
     let answer = [];
-    let cnt = 0;
-    let cntZero = 0;
+    let deleteZero = 0;
+    let count = 0;
     
     while(s !== '1') {
         let len = 0;
         for (let i = 0; i < s.length; i++) {
-        if (s[i] === '1') {
-            len++;
-        } else {
-            cntZero++;
+            if (s[i] === '1') {
+                len++;
+            } else {
+                deleteZero++;
+            }
         }
+        
+        s = len.toString(2);
+        count++;
     }
-    s = len.toString(2);
-    cnt++
-    }
-   
-    answer.push(cnt, cntZero)
-    return answer;
+    
+    return [count, deleteZero];
 }
