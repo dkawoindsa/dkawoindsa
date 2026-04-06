c42b4adb7a53ebac2b496598d039145240a2987a
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-04-06 17:20:53 +0900

    [level 2] Title: 숫자의 표현, Time: 0.32 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,9 +1,13 @@
 function solution(n) {
+    // n의 약수 중 홀수의 개수를 구하는 문제
     let answer = 0;
+
     for (let i = 1; i <= n; i++) {
-        if ((n % i == 0) && (i % 2 == 1)) {
+        if (n % i === 0 && i % 2 === 1) {
             answer++;
         }
     }
+    
+    
     return answer;
 }
