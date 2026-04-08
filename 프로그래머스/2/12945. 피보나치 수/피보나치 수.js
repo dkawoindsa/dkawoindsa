251eb59286a67a337d79969857f5bdf4516d48d1
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-04-08 17:24:24 +0900

    [level 2] Title: 피보나치 수, Time: 2.46 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,11 +1,12 @@
 function solution(n) {
-    let prev = 0;
-    let prev2 = 1;
+    let prv = 0;
+    let prv2 = 1;
     
     for (let i = 2; i <= n; i++) {
-        const next = (prev + prev2) % 1234567;
-        prev = prev2;
-        prev2 = next;
+        let next = (prv + prv2) % 1234567;
+        prv = prv2;
+        prv2 = next;
     }
-    return prev2;
+    
+    return prv2;
 }
