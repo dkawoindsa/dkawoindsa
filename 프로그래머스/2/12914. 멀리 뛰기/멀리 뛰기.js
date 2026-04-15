74d002e96065023c0a460401ee044471db5123af
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-04-15 16:20:17 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,16 +1,14 @@
 function solution(n) {
-    let a = 1;
-    let b = 2;
+
+    let first = 1;
+    let second = 2;
+    
     
     for (let i = 3; i <= n; i++) {
-        let temp = (a + b) % 1234567;
-        a = b;
-        b = temp;
-        
+        let temp = (first + second) % 1234567
+        first = second;
+        second = temp;
     }
-    return n === 1 ? 1 : b;
+    
+    return n === 1 ? 1 : second;
 }
-
-
-
-
