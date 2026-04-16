a0b6439ad72de9aa0d0db1d32f313f6268ab0a83
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-04-16 17:59:44 +0900

    [level 2] Title: 예상 대진표, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,12 +1,15 @@
 function solution(n,a,b)
 {
-    let answer = 0;
-    
-    while(a !== b) {
+    let answer = 0;   
+
+    for (let i = 0; i < n; i++) {
         a = Math.ceil(a / 2);
         b = Math.ceil(b / 2);
-        answer += 1;
+        answer++;
+        
+        if (a === b) {
+            return answer;
+        }
     }
-
-    return answer;
+    
 }
