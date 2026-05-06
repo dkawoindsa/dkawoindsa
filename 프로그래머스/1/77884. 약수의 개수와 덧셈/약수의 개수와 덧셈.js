f67bb44ada95ce8b9255d5684d23e356ce694410
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-05-06 17:57:11 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.91 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,22 +1,20 @@
 function solution(left, right) {
     let answer = 0;
-    let countDiv = 0;
+    
     
     for (let i = left; i <= right; i++) {
+        let countDiv = 0;
         for (let j = 1; j <= i; j++) {
             if (i % j === 0) {
                 countDiv++;
             }
         }
         
-    if (countDiv % 2 === 0) {
-        answer += i;
-    } else {
-        answer -= i;
-    }
-        
-    countDiv = 0;
-        
+        if (countDiv % 2 === 0) {
+            answer += i;
+        } else {
+            answer -= i;
+        }
     }
     
     return answer;
