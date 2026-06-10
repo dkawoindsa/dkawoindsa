ffdb4e1296725b0f73a8d70192b5338e1c4cb75d
dkawoindsa <145523906+dkawoindsa@users.noreply.github.com>
2026-06-10 12:29:14 +0900

    [level 1] Title: 콜라 문제, Time: 0.23 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,11 @@
+function solution(a, b, n) {
+    let answer = 0;
+    
+    while (n >= a) {
+        const temp = Math.floor(n / a) * b;
+        answer += temp;
+        n = temp + (n % a);
+    }
+    
+    return answer;
+}
